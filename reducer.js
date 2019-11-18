6d19385a0fe659a664fa50267aaa151daf765e78
Ricartho Franck <ricartho.franck@esih.edu>
2019-11-18 10:24:00 +0000

    Done.

--- reducer.js
+++ reducer.js
@@ -7,9 +7,22 @@ function changeState(state, action){
       return state;
   }
 }
+<<<<<<< HEAD
 render = () => (document.body.textContent = state.count)
+=======
+
+render = () => (document.body.textContent = state.count)
+
+>>>>>>> 46a14cf5ce68e869d3ceae526451c5490534bd42
 function dispatch(action){
   state = changeState(state,action)
   render()
 }
+<<<<<<< HEAD
 render()
+=======
+
+render()
+
+
+>>>>>>> 46a14cf5ce68e869d3ceae526451c5490534bd42
